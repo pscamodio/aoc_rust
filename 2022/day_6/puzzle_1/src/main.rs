eff4f96ce4988601601cdac354bd618352e9f0fe
Amodio Pesce <pscamodio@gmail.com>
2022-12-12 13:05:27 +0100

    AOC2022 - day 6- puzzle 1 and 2

---
+++ 2022/day_6/puzzle_1/src/main.rs
@@ -0,0 +1,39 @@
+use std::{error::Error, fs};
+
+fn has_duplicates(s: &str) -> bool {
+    for c in s.chars() {
+        if s.matches(c).count() > 1 {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("2022/day_6/input.txt")?;
+
+    let mut iter = input.chars();
+
+    let mut marker: String = String::new();
+
+    let mut idx = 0;
+
+    loop {
+        let next = iter.next().unwrap();
+
+        marker.push(next);
+
+        if marker.len() > 4 {
+            marker.drain(0..1);
+        }
+
+        if marker.len() == 4 && !has_duplicates(&marker) {
+            println!("End of marker: {}", idx + 1);
+            break;
+        }
+
+        idx += 1;
+    }
+
+    Ok(())
+}
