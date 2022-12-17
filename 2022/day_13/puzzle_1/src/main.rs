cffc89a58113f527714e3fcf1746e65821e4f6d1
Amodio Pesce <amodio.pesce@faro.com>
2022-12-17 21:37:54 +0100

    AOC2022 - day 13 - puzzle 1

--- 2022/day_13/puzzle_1/src/main.rs
+++ 2022/day_13/puzzle_1/src/main.rs
@@ -1,12 +1,36 @@
 use std::{
     error::Error,
-    fs, str::Chars, iter::Peekable
+    fs, str::Chars, fmt::Display
 };
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Value {
     Value(i32),
     List(Vec<Value>)
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match (self, other) {
+            (Value::Value(a), Value::Value(b)) => a.partial_cmp(b),
+            (Value::List(a), Value::List(b)) => a.partial_cmp(b),
+            (Value::Value(a), Value::List(b)) => vec![Value::Value(*a)].partial_cmp(&b),
+            (Value::List(a), Value::Value(b)) => a.partial_cmp(&vec![Value::Value(*b)]),
+        }
+    }
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::Value(x) => write!(f, "{x}"),
+            Value::List(x) => {
+                let res : Vec<String> = x.iter().map(|x| x.to_string()).collect();
+                let res = res.join(",");
+                write!(f, "[{res}]")
+            }
+        }   
+    }
 }
 
 impl Value {
@@ -29,14 +53,14 @@ impl Value {
         (str.parse().unwrap(), array_end)
     }
 
-    fn parse(itr: &mut Chars) -> Vec<Value> {
+    fn parse_list(itr: &mut Chars) -> Vec<Value> {
 
-        let mut value = Vec::new();
+        let mut value: Vec<Value> = Vec::new();
 
         while let Some(c) = itr.next() {
             match c {
                 '[' => {
-                    value.push(Value::List(Value::parse(itr)));
+                    value.push(Value::List(Value::parse_list(itr)));
                 },
                 ',' => {},
                 ']' => {
@@ -54,20 +78,34 @@ impl Value {
 
         value
     }
+
+    fn parse(itr: &mut Chars) -> Value {
+        let first = itr.next().unwrap();
+
+        if first != '[' {
+            Value::Value(Value::parse_number(first, itr).0)
+        } else {
+            Value::List(Value::parse_list(itr))
+        }
+
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("2022/day_13/input_simple.txt")?;
 
-    for line in input.lines() {
-        println!("");
-        if line.len() < 3 {
-            continue;
+    let mut sum = 0;
+
+    for (idx, block) in input.split("\n\n").enumerate() {
+        let mut lines = block.lines();
+        let val1 = lines.next().map(|x| Value::parse(&mut x.chars())).unwrap();
+        let val2 = lines.next().map(|x| Value::parse(&mut x.chars())).unwrap();
+        println!("{}", val1);
+        println!("{}", val2);
+        println!("a < b = {}", val1 < val2);
+        if val1 < val2 {
+            sum += idx + 1;
         }
-        let mut chars = line.chars();
-        chars.next();
-        let val = Value::parse(&mut chars);
-        println!("{val:?}");
     }
 
 
--- 2022/day_13/puzzle_2/src/main.rs
+++ 2022/day_13/puzzle_2/src/main.rs
@@ -1,10 +1,128 @@
 use std::{
     error::Error,
-    fs
+    fs, str::Chars, fmt::Display
 };
 
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum Value {
+    Value(i32),
+    List(Vec<Value>)
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match (self, other) {
+            (Value::Value(a), Value::Value(b)) => a.partial_cmp(b),
+            (Value::List(a), Value::List(b)) => a.partial_cmp(b),
+            (Value::Value(a), Value::List(b)) => vec![Value::Value(*a)].partial_cmp(&b),
+            (Value::List(a), Value::Value(b)) => a.partial_cmp(&vec![Value::Value(*b)]),
+        }
+    }
+}
+
+impl Ord for Value {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(&other).unwrap()
+    }
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::Value(x) => write!(f, "{x}"),
+            Value::List(x) => {
+                let res : Vec<String> = x.iter().map(|x| x.to_string()).collect();
+                let res = res.join(",");
+                write!(f, "[{res}]")
+            }
+        }   
+    }
+}
+
+impl Value {
+    fn parse_number(first: char, itr: &mut Chars) -> (i32, bool) {
+        let mut str = first.to_string();
+        let mut array_end = false;
+        for c in itr {
+            match c {
+                ']' => {
+                    array_end = true;
+                    break;
+                },
+                ',' => {
+                    array_end = false;
+                    break;
+                }
+                x => str.push(x),
+            }
+        }
+        (str.parse().unwrap(), array_end)
+    }
+
+    fn parse_list(itr: &mut Chars) -> Vec<Value> {
+
+        let mut value: Vec<Value> = Vec::new();
+
+        while let Some(c) = itr.next() {
+            match c {
+                '[' => {
+                    value.push(Value::List(Value::parse_list(itr)));
+                },
+                ',' => {},
+                ']' => {
+                    break;
+                }
+                _ => {
+                    let (num, end) = Value::parse_number(c, itr);
+                    value.push(Value::Value(num));
+                    if end {
+                        break;
+                    }
+                }
+            }
+        }
+
+        value
+    }
+
+    fn parse(itr: &mut Chars) -> Value {
+        let first = itr.next().unwrap();
+
+        if first != '[' {
+            Value::Value(Value::parse_number(first, itr).0)
+        } else {
+            Value::List(Value::parse_list(itr))
+        }
+
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = fs::read_to_string("2022/day_12/input.txt")?;
+    let input = fs::read_to_string("2022/day_13/input.txt")?;
+
+    let ref1 = Value::parse(&mut "[[2]]".chars());
+    let ref2 = Value::parse(&mut "[[6]]".chars());
+
+    let mut packets = vec![ref1.clone(), ref2.clone()];
+
+    for block in input.split("\n\n") {
+        let mut lines = block.lines();
+        let val1 = lines.next().map(|x| Value::parse(&mut x.chars())).unwrap();
+        let val2 = lines.next().map(|x| Value::parse(&mut x.chars())).unwrap();
+        packets.push(val1);
+        packets.push(val2);
+    }
+
+    packets.sort();
+
+    for (idx, packet) in packets.iter().enumerate() {
+        println!("{}: {packet}", idx + 1);
+    }
+
+    let idx1 = packets.iter().position(|x| *x == ref1).unwrap() + 1;
+    let idx2 = packets.iter().position(|x| *x == ref2).unwrap() + 1;
+
+    println!("Result {}", idx1*idx2);
 
     Ok(())
 }
